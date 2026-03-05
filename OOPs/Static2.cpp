fbefccf7925ae2c9871945e5b613760986cb943f
TrikamDevasi <trikam.devasi.cg@outlook.com>
2026-03-05 10:53:40 +0530

    static updated

--- OOPs/Static2.cpp
+++ OOPs/Static2.cpp
@@ -12,19 +12,29 @@ class CreateObj{
       static int count;
       static float num;
       static string name;
+      int data = 0;
       
     // constructor....
     
-      CreateObj(){
+      CreateObj(int data){
           ++count;
+          this->data = data;
           cout << "The count is incremented to trace the number of objects.." << endl;
       }
       
-    // Function members...
+    // Function members it is non-static...
     
       void display(){
-          cout << "Total numbers of object created for this class is ->" << CreateObj::count << endl;
-          cout << "The static name of this class is:" << CreateObj::name << endl;
+        //   cout << "Total numbers of object created for this class is ->" << CreateObj::count << endl;
+        //   cout << "The static name of this class is:" << CreateObj::name << endl;
+          cout << "The non satic data members is: " << this->data << endl;
+      }
+      
+    // Function member but it is static...
+    
+      static void display1(CreateObj& obj){
+          cout <<"The display1 fucntion is a static fucntion belongs to a class not to an object" << endl;
+          obj.display();
       }
       
     // Destrucotr to trace those objects which are going to destroy....
@@ -42,43 +52,45 @@ string CreateObj :: name = "Samir";
 
 void create(){
     cout << "Total 5 object will be created.." << endl;
-    CreateObj obj4;
-    obj4.display();
+    CreateObj obj4(4);
+    // obj4.display();
     
-    CreateObj obj5;
-    obj5.display();
+    CreateObj obj5(5);
+    // obj5.display();
     
-    CreateObj obj6;
-    obj6.display();
+    CreateObj obj6(6);
+    // obj6.display();
     
-    CreateObj obj7;
-    obj7.display();
+    CreateObj::display1(obj6);
     
-    CreateObj obj8;
-    obj8.display();
+    // CreateObj obj7;
+    // obj7.display();
+    
+    // CreateObj obj8;
+    // obj8.display();
 
     cout <<"All object which is created is about to get destroyed becauise of its life scope" << endl;
 }
 
 int main(){
     
-    cout << "The object will start creating from this point" << endl;
+    // cout << "The object will start creating from this point" << endl;
     
-    CreateObj obj1;
-    obj1.display();
+    // CreateObj obj1;
+    // obj1.display();
     
-    cout << "Another object is created..." << endl;
-    CreateObj obj2;
-    obj2.display();
+    // cout << "Another object is created..." << endl;
+    // CreateObj obj2;
+    // obj2.display();
     
-    cout << "Another object is created..." << endl;
-    CreateObj obj3;
-    obj3.display();
+    // cout << "Another object is created..." << endl;
+    // CreateObj obj3;
+    // obj3.display();
 
-    cout << "Function is called to create extra objects.." << endl;
+    // cout << "Function is called to create extra objects.." << endl;
     create();
     
-    
-    obj2.display();
+    // obj2.display();
+
     return 0;
 }
